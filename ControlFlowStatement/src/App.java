8430709341a4fd05f78ed4cf177549b2b586ae2b
bramwell simiyu <simiyub.engineer@gmail.com>
2020-08-14 09:42:33 +0100

    learning switch

---
+++ ControlFlowStatement/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        Switch switchlesson = new Switch();
+        switchlesson.switchLesson();
+        switchlesson.challengeChar();
+        switchlesson.switchString();
+        switchlesson.challengeString(4);
+        switchlesson.challengeStringBonus(6);
+    }
+}
