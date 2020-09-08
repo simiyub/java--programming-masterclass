ca0eb33920d49c90b1599c6896baa800f5b12a6d
bramwell simiyu <simiyub.engineer@gmail.com>
2020-09-08 08:48:08 +0100

    refactor

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -2,85 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i=1; i<11; i++){
-            Movie movie=randomMovie();
+            Movie movie=Movie.randomMovie();
             System.out.println("Movie #"+ i +"\n"+ "Plot:"+movie.plot()+"\n");
         }
     }
-    public static Movie randomMovie(){
-        int randomMovie=(int)(Math.random()*5)+1;
-        System.out.println("Random number was");
 
-        //
-        switch(randomMovie){
-            case 1:
-            return new Jaws();
-            case 2:
-                return new IndependenceDay();
-            case 3:
-                return new MazeRunner();
-            case 4:
-                return new StarWars();
-            case 5:
-                return new Forgetable();
-        }
-        return null;
-            }
-}
-
-class Movie{
-    private String name;
-
-    public Movie(String name){
-
-    }
-    public String plot() {
-        return "No plot";
-    }
-}
-
-class Jaws extends Movie {
-    public Jaws() {
-        super("Jaws");
-    }
-    @Override
-    public String plot(){
-        return "A shark eats lots of people";
-    }
-}
-
-class IndependenceDay extends Movie{
-    public IndependenceDay(){
-        super("Independence Day");
-    }
-    @Override
-    public String plot(){
-        return "Aliens attempt to take over planet earth.";
-    }
-}
-
-class MazeRunner extends Movie{
-    public MazeRunner(){
-        super("Maze runner");
-    }
-    @Override
-    public String plot(){
-        return "Kids try to escape a maze.";
-    }
-}
-
-class StarWars extends Movie{
-    public StarWars (){
-        super("Star Wars");
-    }
-
-    @Override
-    public String plot() {
-        return "Imperial Forces try to take over the universe.";
-    }
-}
-
-class Forgetable extends Movie{
-    public Forgetable(){
-        super("Forgetable");
-    }
 }
