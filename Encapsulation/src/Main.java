bed3a4f437da754fb5c81a70fdd89f4415b4e429
bramwell simiyu <simiyub.engineer@gmail.com>
2020-09-08 08:06:12 +0100

    encapsulation example

--- Encapsulation/src/Main.java
+++ Encapsulation/src/Main.java
@@ -1,5 +1,7 @@
 import data.PlayerEncapsulated;
 import data.PlayerNoEncapsulation;
+import data.Printer;
+import data.Toner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +14,11 @@ public class Main {
 
         PlayerEncapsulated playerEncapsulated = new PlayerEncapsulated("Peter",10,"sword");
         playerEncapsulated.loseHealth(3);
+
+        Printer printer = new Printer("lexmark", "121",true);
+        printer.addToner(new Toner("black",100));
+        printer.print(100);
+        printer.print(500);
+        printer.print(99400);
     }
 }
